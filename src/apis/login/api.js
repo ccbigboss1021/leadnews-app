998d6a9833c86e005a8074d41ab9f6f15d19a4a8
zRain <2296342883@qq.com>
2023-04-05 00:35:34 +0800

    feat: 🎸 add register

--- src/apis/login/api.js
+++ src/apis/login/api.js
@@ -1,15 +1,20 @@
-function Api(){
-    this.vue;
+function Api() {
+  this.vue
 }
 Api.prototype = {
-    setVue : function(vue){
-        this.vue = vue;
-    },
-    // 登录
-    login: function(data){
-        let url = this.vue.$config.urls.get('user_login')
-        return this.vue.$request.postByEquipmentId(url,data)
-    }
+  setVue: function (vue) {
+    this.vue = vue
+  },
+  // 登录
+  login: function (data) {
+    let url = this.vue.$config.urls.get('user_login')
+    return this.vue.$request.postByEquipmentId(url, data)
+  },
+
+  // 注册
+  register: function (data) {
+    return this.vue.$request.postByEquipmentId('/api/v1/login/register', data)
+  },
 }
 
 export default new Api()
